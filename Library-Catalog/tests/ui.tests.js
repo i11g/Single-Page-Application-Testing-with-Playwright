8b13407dc7ee877daa678dff5014cec844d6232c
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-14 12:03:04 +0300

    files changed

--- Library-Catalog/tests/ui.tests.js
+++ Library-Catalog/tests/ui.tests.js
@@ -1,1 +1,12 @@
 import {test, expect} from '@playwright/test' 
+
+//Navigation
+test('Verify "All books" link is visible', async({page})=> {
+    
+    await page.goto('http://localhost:3000/catalog')
+    
+    await page.waitForSelector(nav.navbar)
+    const allBookLink=await page.$('a[href="/catalog');
+    const isLinkVisible= await allBookLink.isVisible(); 
+    expect(isLinkVisible).toBe(true);
+})
