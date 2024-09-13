0f948ced0c50d45973643794ec7c37f57d4ce735
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-09-13 06:12:40 +0300

    new files added

--- Meme Application/02-JS-App-For-Testing/api/requests.js
+++
@@ -1,7 +0,0 @@
-import { user } from "./users.js";
-import { items } from "./items.js";
-
-export const requests = {
-    user,
-    items
-};
--- My Theater/api/requests.js
+++
@@ -1,9 +0,0 @@
-import { user } from "./users.js";
-import { theaters } from "./theaters.js";
-import { likes } from "./likes.js";
-
-export const requests = {
-    user,
-    theaters,
-    likes
-};
